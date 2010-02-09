f7fd6251be28595e9d403f5e8bc1017988735479
Shaun Sephton <shaun@praekeltconsulting.com>
2010-02-09 15:19:34 +0200

    properly assign choices avoiding admin wonkyness

--- publisher/admin.py
+++ publisher/admin.py
@@ -13,22 +13,24 @@ class PublisherAdmin(admin.ModelAdmin):
     )
 
 class ViewAdminForm(forms.ModelForm):
+    page = forms.ChoiceField()
+    
     def __init__(self, *args, **kwargs):
         super(ViewAdminForm, self).__init__(*args, **kwargs)
-        self.fields['page'].choices += self.get_page_choices()
+        self.fields['page'].choices = self.get_page_choices()
 
     def get_page_choices(self):
         """
         generate page choices from url patterns
         """
         root_urlconf = __import__(settings.ROOT_URLCONF, globals(), locals(), ['urlpatterns',])
-        page_choices = []
+        page_choices = [(None, '---------'),]
         for pattern in root_urlconf.urlpatterns:
             try:
                 page_choices.append((pattern.name, pattern.name.title().replace('_', ' ')))
             except AttributeError:
                 pass
         return page_choices
-
+    
 class ViewAdmin(admin.ModelAdmin):
     form = ViewAdminForm
