8a7b3e9e0c43450f03b26bdf016c74b2a24a3f7d
Shaun Sephton <shaun@praekelt.com>
2010-03-02 21:16:14 +0200

    now limits publishing targets based on widget restricted_to fields

--- publisher/admin.py
+++ publisher/admin.py
@@ -1,8 +1,26 @@
 from django import forms
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.admin.options import InlineModelAdmin
+
+
+class PublisherAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PublisherAdminForm, self).__init__(*args, **kwargs)
+        self.fields['targets'].choices = self.get_target_choices()
+
+    def get_target_choices(self):
+        from publisher.models import Widget
+        app_model_label = '.'.join((self._meta.model._meta.app_label, self._meta.model._meta.object_name))
+        widgets = Widget.objects.all()
+        choices = []
+        for widget in widgets:
+            if app_model_label in widget.as_leaf_class().restricted_to:
+                choices.append((widget.id, str(widget)))
+        return choices
 
 class PublisherAdmin(admin.ModelAdmin):
+    form = PublisherAdminForm
     list_display = ('is_public',)
     list_filter = ('is_public',)
     fieldsets = (
@@ -11,6 +29,15 @@ class PublisherAdmin(admin.ModelAdmin):
             'classes': ('collapse',),
         }),
     )
+
+class PublisherInlineModelAdmin(InlineModelAdmin):
+    form = PublisherAdminForm
+
+class PublisherStackedInline(PublisherInlineModelAdmin, admin.StackedInline):
+    pass
+
+class PublisherTabularInline(PublisherInlineModelAdmin, admin.TabularInline):
+    pass
 
 class ViewAdminForm(forms.ModelForm):
     page = forms.ChoiceField()
