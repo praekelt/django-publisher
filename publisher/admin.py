6582a05b11ff0e0cc4e39cbea9552bb018a11bdd
Shaun Sephton <shaun@praekelt.com>
2010-04-19 13:41:12 +0200

    added sites and publisher

---
+++ publisher/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from publisher.models import Buzz, Facebook, Mobile, SocialBookmark, Twitter, Web
+
+admin.site.register(Buzz)
+admin.site.register(Facebook)
+admin.site.register(Mobile)
+admin.site.register(SocialBookmark)
+admin.site.register(Twitter)
+admin.site.register(Web)
