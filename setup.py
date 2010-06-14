81c5d5a1b682a3591928bf223ff4b4e651e68bac
shaunsephton <shaunsephton@gmail.com>
2010-06-14 18:46:46 +0200

    polish

--- setup.py
+++ setup.py
@@ -2,13 +2,28 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-publisher',
-    version='dev',
+    version='0.0.1',
     description='Django external publishing app.',
-    author='Praekelt Consulting',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-publisher',
+    license='BSD',
+    url='http://github.com/praekelt/django-publisher',
     packages = find_packages(),
-    dependency_links = ['http://github.com/sciyoshi/pyfacebook/tarball/master#egg=pyfacebook',],
-    install_requires = ['pyfacebook',],
+    dependency_links = [
+        'http://github.com/sciyoshi/pyfacebook/tarball/master#egg=pyfacebook',
+    ],
+    install_requires = [
+        'pyfacebook',
+    ],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
