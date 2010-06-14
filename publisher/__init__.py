0a79794ac7b75c6cb22eb0545f5874e43bd3beea
shaunsephton <shaunsephton@gmail.com>
2010-06-14 18:35:48 +0200

    hook up publishing

---
+++ publisher/__init__.py
@@ -0,0 +1,11 @@
+from django.db.models import signals
+from publisher.models import Publisher
+
+def publish(sender, instance, action, reverse, model, pk_set, **kwargs):
+    if model == Publisher:
+        if pk_set and action == 'post_add':
+            publishers = Publisher.objects.filter(pk__in=pk_set)
+            for publisher in publishers:
+                publisher.publish(instance)
+
+signals.m2m_changed.connect(publish)
