da5368e50432653dd3cf61a1ccec22b66b911d56
shaunsephton <shaunsephton@gmail.com>
2010-06-14 18:32:30 +0200

    added pyfacebook dep

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     author_email='dev@praekelt.com',
     url='https://github.com/praekelt/django-publisher',
     packages = find_packages(),
+    dependency_links = ['http://github.com/sciyoshi/pyfacebook/tarball/master#egg=pyfacebook',],
+    install_requires = ['pyfacebook',],
     include_package_data=True,
 )
