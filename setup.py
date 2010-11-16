102d7391b9f431a63fd172c9ae4d815e295382aa
shaunsephton <shaun@praekeltconsulting.com>
2010-11-16 14:02:22 +0200

    dev ver rev

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-publisher',
-    #version='dev',
-    version='mtv.southafrica.portal.vlive.0.0.1',
+    version='dev',
+    #version='mtv.southafrica.portal.vlive.0.0.1',
     description='Django targeted publishing through view layouts and widgets app.',
     author='Praekelt Consulting',
     author_email='dev@praekelt.com',
