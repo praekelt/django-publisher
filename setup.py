fd6dfd27517ff1fb61d60ead7bc7232d83d6f72a
shaunsephton <shaun@praekeltconsulting.com>
2010-11-19 12:55:36 +0200

    versioned. zip safe false

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-publisher',
-    version='dev',
+    version='apps-api.0.0.1',
     description='Django targeted publishing through view layouts and widgets app.',
     author='Praekelt Consulting',
     author_email='dev@praekelt.com',
     url='https://github.com/praekelt/django-publisher',
     packages = find_packages(),
     include_package_data=True,
+    zip_safe=False,
 )
